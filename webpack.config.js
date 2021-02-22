8a600b2286df158a5a8c15c4d77dbe681fcd38bd
Daria <44212818+DariaGunina@users.noreply.github.com>
2021-02-22 15:18:38 +0300

    Revert "Master"

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-function buildConfig() {
-    const env = process.env.NODE_ENV;
-    if (env === 'dev' || env === 'prod') {
-        return require(`./webpack.${env}.js`);
-    } else {
-        console.log("Wrong webpack build parameter. Possible choices: 'dev' or 'prod'.")
-    }
-}
-module.exports = buildConfig;

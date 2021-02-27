ea6ce8dde88d60b7eb31324b3e0028d0d3f98d3b
Dariia_Gunina <dariia_gunina@epam.com>
2021-02-27 15:24:50 +0300

    Task 2: fixed message error

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+const DEV = 'dev';
+const PROD = 'prod';
+
 function buildConfig() {
     const env = process.env.NODE_ENV;
     try {
         return require(`./webpack.${env}.js`);
     } catch (e) {
-        console.log("Wrong webpack build parameter. Possible choices: 'dev' or 'prod'.");
+        console.log(`Wrong webpack build parameter. Possible choices: ${DEV} or ${PROD}.`);
     }
 };
 
